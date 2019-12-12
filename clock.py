7d9babeaadb22b4e713e9e25adf93976f37daffc
unknown <Sukrit>
2019-12-12 09:30:08 +0530

    BA bought

--- clock.py
+++ clock.py
@@ -12,7 +12,7 @@ logging.basicConfig()
 sched = BlockingScheduler()
 
 
-@sched.scheduled_job('interval', minutes=10)
+#@sched.scheduled_job('interval', minutes=10)
 
 
 #@sched.scheduled_job('cron', day_of_week='mon-fri', hour=4, minute=55)
@@ -25,7 +25,7 @@ def timed_job():
 	sell.main()
 	print('This job is run every sixty minutes.')
 
-@sched.scheduled_job('cron', day_of_week='mon-thu,sun', hour=12, minute=30)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=12, minute=30)
 def scheduled_job3():
 	storeFinancials.main()
 	print 'This job is run every weekday at 18:00.'
@@ -35,17 +35,17 @@ def scheduled_job3():
 #	ScoreBuyStocks.main()
 #	print 'This job is run every weekday at 19:00 pm IST.'
 	
-@sched.scheduled_job('cron', day_of_week='mon-thu,sun', hour=21, minute=30)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=03, minute=00)
 def scheduled_job2():
 	ScoreBuyStocks.main()
-	print 'This job is run every weekday at 3:00 am.'
+	print 'This job is run every weekday at 8:30 am.'
 	
-@sched.scheduled_job('cron', day_of_week='mon-thu,sun', hour=17, minute=00)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=17, minute=00)
 def scheduled_job3():
 	storeRatios.main()
 	print 'This job is run every weekday at 22:30.'
 	
-@sched.scheduled_job('cron', day_of_week='mon-thu,sun', hour=21, minute=00)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=21, minute=00)
 def scheduled_job4():
 	Median.main()
 	print 'This job is run every weekday at 2:30 am.'
