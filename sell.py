40abfc1786eb085598f36298b4588cbd0f527047
unknown <Sukrit>
2020-01-11 19:53:28 +0800

    Changing broker rates

--- sell.py
+++ sell.py
@@ -21,13 +21,14 @@ def getAnnualReturn(currentPrice, purchasePrice, purchaseDate):
 	deltaPer = float(delta.days + 1)/365.0
 	return pow(ROI,1/deltaPer)
 
-def getReturn(currentPrice, purchasePrice, purchaseDate):
-	ROI  = currentPrice*.995/purchasePrice
+def getReturn(currentPrice, purchasePrice):
+	#removing broker charges
+	ROI  = currentPrice*.9943/purchasePrice*1.0057
 	return ROI
 
 def shouldSell(currentPrice, purchasePrice, purchaseDate, quantity):
-	#sell if annual return is more than 115%
-	if ((currentPrice - purchasePrice)*quantity > 550.0):
+	#sell if annual return is more than 105%
+	if getReturn(currentPrice, purchasePrice) > 1.05:
 		return 1
 	
 	#sell if annual return is less than 90%
